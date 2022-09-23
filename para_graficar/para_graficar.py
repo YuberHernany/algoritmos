1b385327546e741c78bc91f416507fef191ca411
YuberHernany <yuber.tapias@udea.edu.co>
2022-09-23 12:09:51 -0500

    se modifica función graficar, para aceptar en su dominio, listas con 3 entradas, indicando número de puntos

--- para_graficar/para_graficar.py
+++ para_graficar/para_graficar.py
@@ -9,10 +9,17 @@ def ejes():
     ax.spines['left'].set_position("zero")
 
 def graficar(f, dominio, **kwargs):
+    """Entradas:
+            f: expresión de sympy
+            dominio: lista o array [star, stop] o [star, stop, steps]"""
     ax = plt.gca()
     F = sy.lambdify(x, f, "numpy")
-    x_s = np.linspace(dominio[0], dominio[1])
-    ax.plot(x_s, F(x_s), **kwargs)
-
+    if len(dominio) == 2:
+        x_s = np.linspace(dominio[0], dominio[1])
+        ax.plot(x_s, F(x_s), **kwargs)
+    elif len(dominio) == 3:
+        x_s = np.linspace(dominio[0], dominio[1], dominio[2])
+        ax.plot(x_s, F(x_s), **kwargs)
+    
 if __name__ == "__main__":
     pass 
